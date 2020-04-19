6eea60d07fd5d498c8c5f999693dd765e44a275d
Vinzenz Aubry <vinzenzaubry@gmail.com>
2020-04-19 17:11:21 +0200

    added db sketches

---
+++ _codeSketches/001_DB Structure/database sketch.js
@@ -0,0 +1,30 @@
+// colorDB
+const colorDB = {
+  claimedColors: {
+    '0,0,0': {
+      claimed: true,
+      generationTimestamp: 1587306186910,
+    },
+    '255,255,255': {
+      claimed: true,
+      generationTimestamp: 1587306186910,
+    },
+  },
+};
+
+// user & message DB
+const colorDataDB = {
+  '0,0,0': {
+    loginToken: 'rhvlkshsrgakjhgrlkajhglk', // this is for autologin
+    generationTimestamp: 1587306186910,
+    messages: {
+      inboxLostAndFound: [
+        {
+          senderId: '233,233,233',
+          timestamp: 1587306186910,
+          message: 'hi, nice to meet you',
+        },
+      ],
+    },
+  },
+};
