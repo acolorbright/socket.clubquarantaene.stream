67b809e9d33e1999e3f0dc01d031dda32cf1bdca
Vinzenz Aubry <vinzenzaubry@gmail.com>
2020-04-23 02:04:39 +0200

    fixed same port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const ip = process.env.IP || '127.0.0.1';
 const server = http.createServer(app);
 const socketPort = process.env.SOCKETPORT || 1337;
 
-const io = require('socket.io')(1337);
+const io = require('socket.io')(server);
 // eslint-disable-next-line no-unused-vars
 const sockets = require('./api/chat')(io);
 
